1e07e208f1324da4bfd7a0043388ee6be18b532a
Kaique Rocha <kaiquerocha@MacBook-Air-de-Kaique.local>
2024-03-24 15:33:14 -0300

    Terraform planejado

--- eks_nodegroup.tf
+++ eks_nodegroup.tf
@@ -15,7 +15,7 @@ resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "lab-kaique-node-grpup"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids      = [aws_subnet.lab-public-a, aws_subnet.lab-public-b]
+  subnet_ids      = [aws_subnet.lab-public-a.id, aws_subnet.lab-public-b.id]
   capacity_type   = "SPOT"
   disk_size       = "20"
   instance_types  = ["t2.small"]
